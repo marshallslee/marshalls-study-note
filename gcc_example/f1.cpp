1d85d9518aca00d38b6283e8c088ebfc479e7dab
Marshall Lee <marshall.s.lee@gmail.com>
2022-05-16 10:41:51 +0900

    keep learning

---
+++ gcc_example/f1.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+using namespace std;
+
+void f1() {
+	cout << "This is a message from f1" << endl;
+}
---
+++ gcc_example/f2.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+using namespace std;
+
+void f2() {
+	cout << "This is a message from f2" << endl;
+}
